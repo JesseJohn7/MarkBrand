3b81c397c8fd9534150b142d166a75ee6f16f572
JesseJohn7 <jessejohn260@gmail.com>
2026-03-03 15:47:25 +0100

    logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Navbar from "@/components/Navbar";
+import AboutSection from "@/components/About";
 export default function Home() {
   return (
         <>
           <Navbar/>
+          <AboutSection/>
         </>
   );
 }
