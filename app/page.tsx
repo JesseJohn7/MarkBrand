6134aaca1d4255cdcebcdbda9524836f81ad0e8e
JesseJohn7 <jessejohn260@gmail.com>
2026-03-04 12:04:37 +0100

    About and imgs

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import AboutSection from "@/components/About";
+
 export default function Home() {
   return (
         <>
           <Navbar/>
             <Hero/>
             <AboutSection/>
+            <Team/>
         </>
   );
 }
