c287af86625e00484e8989dc1ea44123151d519c
JesseJohn7 <jessejohn260@gmail.com>
2026-03-04 12:08:03 +0100

    teamv1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import AboutSection from "@/components/About";
-
+import Team from "@/components/Team";
 export default function Home() {
   return (
         <>
