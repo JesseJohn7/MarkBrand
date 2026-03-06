133438bb2429a63578b983827a10254d44fb7e4a
JesseJohn7 <jessejohn260@gmail.com>
2026-03-06 09:48:43 +0100

    team import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import AboutSection from "@/components/About";
-import Team from "@/components/Team";
 import Gallery from "@/components/Gallery";
 import Services from "@/components/Services"
 export default function Home() {
@@ -10,7 +9,6 @@ export default function Home() {
           <Navbar/>
             <Hero/>
             <AboutSection/>
-            <Team/>
             <Gallery/>
             <Services/>
         </>
