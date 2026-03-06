66992d5d7ac1235231ea45f19297fe49f44d1854
JesseJohn7 <jessejohn260@gmail.com>
2026-03-06 10:55:51 +0100

    footer import

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import AboutSection from "@/components/About";
 import Gallery from "@/components/Gallery";
 import Services from "@/components/Services"
+import Footer from "@/components/Footer"
 export default function Home() {
   return (
         <>
@@ -11,6 +12,7 @@ export default function Home() {
             <AboutSection/>
             <Gallery/>
             <Services/>
+            <Footer/>
         </>
   );
 }
